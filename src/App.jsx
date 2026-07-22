dbfb4073a5b2ad35cd423ebd7b3cec708e384ed3
Madhuri Peduri <pedurimadhur1@gamil.com>
2026-07-22 11:22:30 +0530

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ const App = () => {
     <div>
         <h1>App Component</h1>
         <h1>hello world</h1>
-        <h1>hey world
-          
-        </h1>
+        <h1>hey world</h1>
 
     </div>
   )
